a7a3572e5b988e8a8e2957c6e806a91a447e580f
Poorva <poorva@Poorvas-MacBook-Air.local>
2024-08-10 00:04:02 -0400

    setup game

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-alert ("Hello ")
+const canvas = document.querySelector ('canvas')
+const c = canvas.getContext('2d')
+const c1 = canvas.getContext('3d')
+
+canvas.width =window.innerWidth
+canvas.height = window.innerHeight 
+
+c.fillStyle = 'black' 
+c.fillRect (0, 0 , canvas.width , canvas.height)
