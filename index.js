17af39624f93099953b4b25bd7e78148cefbbcfe
Poorva <poorva@Poorvas-MacBook-Air.local>
2024-08-12 19:09:14 -0400

    created a player

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
-const canvas = document.querySelector ('canvas')
-const c = canvas.getContext('2d')
-const c1 = canvas.getContext('3d')
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
 
-canvas.width =window.innerWidth
-canvas.height = window.innerHeight 
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-c.fillStyle = 'black' 
-c.fillRect (0, 0 , canvas.width , canvas.height)
+c.fillStyle = 'black';
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+class Player {
+    constructor({ position, velocity }) {
+        this.position = position;
+        this.velocity = velocity;
+    }
+
+    draw() {
+        c.beginPath();
+        // Draw circle
+        c.arc(this.position.x, this.position.y, 5, 0, Math.PI * 2);
+        c.fillStyle = '#b7c7a5';
+        c.fill();
+
+        // Draw triangle
+        c.beginPath();
+        c.moveTo(this.position.x + 30, this.position.y);
+        c.lineTo(this.position.x - 10, this.position.y - 10);
+        c.lineTo(this.position.x - 10, this.position.y + 10);
+        c.closePath();
+        c.strokeStyle = 'white';
+        c.stroke();
+    }
+}
+
+const player = new Player({
+    position: { x: canvas.width / 2, y: canvas.height / 2 },
+    velocity: { x: 0, y: 0 }
+});
+
+player.draw();
+
+console.log(player);
